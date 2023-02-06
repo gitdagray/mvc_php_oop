d3085de492704f40415af8f661c2d4ce8e9fa72c
gitdagray <DaveOnEleven@gmail.com>
2023-02-06 10:41:11 -0600

    update db connection, css

--- config/Database.php
+++ config/Database.php
@@ -1,24 +1,29 @@
 <?php 
     class Database {
-        private $host = 'localhost';
-        private $db_name = 'world';
-        private $username = 'root';
-        //private $password = 'pa55word';
-        private $conn;
+        protected $conn;
 
         public function connect() {
-            $this->conn = null;
+            // instead of $this->conn = null;
+            if ($this->conn) {
+                //connection already exists, return it
+                return $this->conn; 
+            } else {
+                $hostname = 'localhost';
+                $database = 'world';
+                $username = 'root';
+                //$password = 'pa55word';
+                $dsn = "mysql:host={$hostname};dbname={$database}";
 
-            try {
-                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username); 
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                $error_message = 'Database Error: ';
-                $error_message .= $e->getMessage();
-                echo $error_message;
-                exit('Unable to connect to the database');
+                try {
+                    $this->conn = new PDO($dsn, $username); //$password
+                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    return $this->conn;
+                } catch (PDOException $e) {
+                    $error_message = 'Database Error: ';
+                    $error_message .= $e->getMessage();
+                    echo $error_message;
+                    exit('Unable to connect to the database');
+                }
             }
-            return $this->conn;
         }
     }
-
